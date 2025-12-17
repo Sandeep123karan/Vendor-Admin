a8f9c7491b0da12fb4f572fb9ed39057c5088b21
Sandeep123karan <sandeepkaran234@gmail.com>
2025-12-17 12:35:27 +0530

    o

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import VendorLogin from './pages/VendorLogin';
+import Dashboard from './pages/Dashboard';
+import Categories from './pages/Categories';
+import Signup from "./pages/Signup";
+import OrderHistory from './pages/OrderHistory';
+import AdminLayout from './pages/AdminLayout';
+import VendorKYC from "./pages/VendorKYC"; 
 
-function App() {
-  const [count, setCount] = useState(0)
 
+import ProductList from "./pages/ProductList";
+import OrderListPage from "./pages/OrderListPage";
+import Setting from "./pages/Setting";
+import VendorSignup from "./pages/VendorSignup"; // add this import
+
+<Routes>
+  <Route path="/vendorlogin" element={<VendorLogin />} />
+  <Route path="/vendorsignup" element={<VendorSignup />} />  {/* NEW */}
+</Routes>
+
+
+
+
+
+
+
+
+
+function App(){
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/VendorLogin" element = {<VendorLogin/>}/>
+        <Route path="/vendorlogin" element={<VendorLogin />} />
+  <Route path="/vendorsignup" element={<VendorSignup />} />
+  <Route path="/vendor/profile-setup" element={<VendorKYC />} />
+
+
+        <Route path="/VendorLogin" element = {<VendorLogin/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/categories' element={<Categories/>}/>
+        <Route path='/orderhistory' element={<OrderHistory />}/>
+        <Route path='/' element={<Navigate to='/login' replace/>} />
+        <Route path='/AdminLayout' element={<AdminLayout/>} />
+        <Route path='/ProductList' element={<ProductList/>} />
+        <Route path="/admin/order-list" element={<OrderListPage />} />
+          <Route path="/pages/Setting" element={<Setting/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
